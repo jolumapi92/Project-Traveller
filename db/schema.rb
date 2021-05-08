1a093a8cc1b90aee36ea396c6e654f2b7d9cb7a9
Jose Matias <josematias92@gmail.com>
2021-05-08 11:44:01 -0500

    Added Devise for every user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_163543) do
+ActiveRecord::Schema.define(version: 2021_05_08_164207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2021_05_08_163543) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_agents_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_agents_on_reset_password_token", unique: true
   end
 
   create_table "travellers", force: :cascade do |t|
@@ -31,6 +38,13 @@ ActiveRecord::Schema.define(version: 2021_05_08_163543) do
     t.string "nationality"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_travellers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_travellers_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
