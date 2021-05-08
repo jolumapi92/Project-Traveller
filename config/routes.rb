a7fd4470e8e86afaacc5aa022410e5c1ff604d6b
Jose Matias <josematias92@gmail.com>
2021-05-08 15:22:06 -0500

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
   devise_for :travellers
   devise_for :users
   root to: 'pages#home'
+
+  resources :agents, only: [:show]
+
+  resources :travellers, only: [:index, :show] do
+    resources :events
+    resources :activities
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
