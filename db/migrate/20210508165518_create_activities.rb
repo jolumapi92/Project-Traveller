010c5898bf92a3fdb96dc4568cd53d2427661c0e
Jose Matias <josematias92@gmail.com>
2021-05-08 11:55:45 -0500

    added all the models

---
+++ db/migrate/20210508165518_create_activities.rb
@@ -0,0 +1,12 @@
+class CreateActivities < ActiveRecord::Migration[6.0]
+  def change
+    create_table :activities do |t|
+      t.string :name
+      t.string :category
+      t.string :place
+      t.integer :points
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_165059) do
+ActiveRecord::Schema.define(version: 2021_05_08_165518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "place"
+    t.integer "points"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "agents", force: :cascade do |t|
     t.string "name"
