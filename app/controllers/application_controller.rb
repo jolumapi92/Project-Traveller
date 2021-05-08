1705ebb1ddd8c05eb896efde3694724f96dd224c
Jose Matias <josematias92@gmail.com>
2021-05-08 16:32:31 -0500

    changed the signed in form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :position, :category])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :position, :category, :gender, :nationality])
   end
 end
